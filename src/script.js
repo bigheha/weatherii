eb978c95e94225161b6d902905d1dbe0af07459a
bigheha <chispinsky1337@gmail.com>
2023-02-07 21:06:57 +0200

    began work on markdown of the application

---
+++ src/script.js
@@ -0,0 +1,10 @@
+import './reset.css';
+import './styles.css';
+
+async function getWeater() {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Ternopil&units=metric&appid=8c705e01d56c4755ae5c8cb11ada00b5',{mode:'cors'});
+    const weatherInfo = await response.json();
+    console.log(weatherInfo);
+}
+
+getWeater();
